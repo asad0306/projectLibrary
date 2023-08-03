43070d826008d5e24807c38318644e8638a1242b
asad0306 <qaisansari46@gmail.com>
2023-08-03 14:01:23 +0100

    js done

--- library.js
+++ library.js
@@ -1,12 +1,73 @@
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
-    this.author = author;
+    this.author = author; 
     this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    render();
+} 
+
+function render() {
+    let libraryEl = document.querySelector('#library');
+    libraryEl.innerHTML = "";
+    for(let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.setAttribute("class", "book-card");
+        bookEl.innerHTML = `
+            <div class ="card-header">
+            <h3 class="title">${book.title}</h3>
+            <h5 class="author">by ${book.author}</h5>
+            </div>
+            <div class="card-body">
+            <p>${book.pages} pages</p>
+            <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+            <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+            <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+            </div>
+        `;
+        libraryEl.appendChild(bookEl);
+        console.log(myLibrary[i]);
+    }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    console.log(index)
+    render();
 }
 
 function addBookToLibrary() {
-
+    let title = document.querySelector("#title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+    console.log(myLibrary);
+    console.log(newBook);
 }
 
+let newBookbtn = document.querySelector('#new-book-btn');
+newBookbtn.addEventListener("click", function() {
+    console.log("Click Hello");
+
+    let newBookForm = document.querySelector('#new-book-form');
+    newBookForm.style.display = "block";
+})
+
+document.querySelector('#new-book-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+    console.log("Ansari Book Store")
+})
